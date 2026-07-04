4d3cd2f929ca938dbddb91ab0ac2a3403c648b3b
shreshtharoyy <shreshtharoy16@gmail.com>
2026-07-04 23:51:12 +0530

    handle missing resolved dates and resolution values

--- src/ingestion/dataset_mapper.py
+++ src/ingestion/dataset_mapper.py
@@ -1,19 +1,19 @@
 from datetime import datetime
 from src.bug_factory import BugFactory
 from src.bug_report import BugReport
+import pandas as pd
 
 class DatasetMapper:
     @staticmethod
     def map_row(row: dict, project: str)-> BugReport:
-        created_at = (datetime.fromisoformat(row["Created"])
-                      if row.get("Created")
-                      else None
-                      )
+        created_at = datetime.fromisoformat(str(row["Created"]))
         
-        resolved_at = (datetime.fromisoformat(row["Resolved"])
-                      if row.get("Resolved")
-                      else None
-                      )
+        resolved_value = row["Resolved"]
+
+        if pd.notna(resolved_value):
+            resolved_at = datetime.fromisoformat(str(resolved_value))
+        else:
+            resolved_at = None
         
         return BugFactory.create(
             bug_id=str(row["Issue id"]),
