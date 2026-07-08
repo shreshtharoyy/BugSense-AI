6e77b4179e637cbca61d563d1dda53887fdbf3b2
shreshtharoyy <shreshtharoy16@gmail.com>
2026-07-08 15:24:18 +0530

    add batch text embedding support using BGE

--- src/embeddings/text_embedder.py
+++ src/embeddings/text_embedder.py
@@ -1,11 +1,12 @@
 from sentence_transformers import SentenceTransformer
 import numpy as np
+from typing import Union, List
 
 class TextEmbedder:
     def __init__(self, model_name: str = "BAAI/bge-small-en-v1.5") -> None:
         self.model = SentenceTransformer(model_name)
 
-    def encode(self, text:str) -> np.ndarray:
-        embedding = self.model.encode(text, convert_to_numpy=True, normalize_embeddings=True)
+    def encode(self, text:Union[str, List[str]], batch_size: int=16) -> np.ndarray:
+        embedding = self.model.encode(text, batch_size=batch_size, convert_to_numpy=True, normalize_embeddings=True)
 
         return embedding
